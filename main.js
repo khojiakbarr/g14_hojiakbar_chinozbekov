e5410b49dcb6c87b2f009ed2053bce7341391ee4
khojiakbarr <chinozbekov09@gmail.com>
2023-12-14 11:27:13 +0500

    version2

---
+++ main.js
@@ -0,0 +1,5 @@
+const name = document.getElementById('name');
+const phone = document.getElementById('Phone');
+const adres = document.getElementById('adres');
+
+const selekt = document.getElementById('selekt');
